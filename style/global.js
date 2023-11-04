143daeea0458b69cbd52a401f62d31f3d9ad79a9
JudahRobBer <143216510+JudahRobBer@users.noreply.github.com>
2023-11-04 19:43:00 -0400

    fix style sheet centering
    ajs

--- style/global.js
+++ style/global.js
@@ -6,7 +6,7 @@ export const globalStyles = StyleSheet.create({
         height: 50,
       },
     container: {
-        marginLeft: 80,
+        justifyContent: "center",
         height: 120,
         flexDirection: 'row', // This makes the children elements align horizontally
         alignItems: 'center', // This centers the children vertically
@@ -30,6 +30,8 @@ export const globalStyles = StyleSheet.create({
         borderWidth: 1, // Add a border to the box (you can customize border styles)
         padding: 10, // Add padding around the content inside the box
         borderRadius: 10, // Add rounded corners to the box
+        justifyContent: "center"
+
     },
     img: {
         height: 50,
