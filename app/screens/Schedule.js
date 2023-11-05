18c749d0f2b343b2de4382ef6d806834c38afe7c
fanrendejuzi <90998758+fanrendejuzi@users.noreply.github.com>
2023-11-04 20:33:06 -0400

    schedule

--- app/screens/Schedule.js
+++ app/screens/Schedule.js
@@ -5,13 +5,14 @@ import {FIRESTORE_DB, FIREBASE_AUTH} from "../../firebaseConfig"
 import CommitmentForm from './Commitments';
 
 export default function ShowSchedule({navigation}){
-    const currentUser = getAuth().currentUser;
-    const userID = currentUser.userID;
-    const docRef = doc(FIRESTORE_DB, "users", userID)
-    docRef
+    // const currentUser = getAuth().currentUser;
+    // const userID = currentUser.userID;
+    // const docRef = doc(FIRESTORE_DB, "users", userID)
     <View>
-        <Text style = {globalStyles.title}> Schedule </Text>
-        <Text style = {globa}
+        <View style = {globalStyles.container}>
+
+        </View>
+
     </View>
 
 }
